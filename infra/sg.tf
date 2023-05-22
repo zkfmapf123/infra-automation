7f4bb0066e6bf11cca4d3b5fc78e2144f49b4648
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-05-23 00:23:29 +0900

    [ADD] sg

--- infra/sg.tf
+++ infra/sg.tf
@@ -43,6 +43,13 @@ resource "aws_security_group" "ansible_sg" {
     }
 
     ingress {
+        from_port = 5000
+        to_port = 5000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
